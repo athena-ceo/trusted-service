3249500adc28ca0d89c450d0234013c5375a20eb
Joël Milgram <joel@milgram.fr>
2025-06-26 16:50:36 +0200

    Addition of new versions of endpoints for analysing and processing requests, with data extracted from the body of the POST request.

--- src/backend/backend/rest/main.py
+++ src/backend/backend/rest/main.py
@@ -1,5 +1,5 @@
 import json
-from typing import Optional
+from typing import Optional, Any
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -55,3 +55,31 @@ async def analyze(field_values: str, text: str):
 async def handle_case(request: CaseHandlingRequest) -> CaseHandlingDetailedResponse:
     print("********************************** request **********************************")
     return app.api.handle_case(request)
+
+
+
+
+
+API_ROUTE = "/api/v1"
+
+@app.post(f"{API_ROUTE}/analyze", tags=["Services"])
+async def analyze_v1(data: dict) -> dict[str, Any]:
+    # Extract the field values from the POST request body
+    field_values: dict[str, Any]= data.get("field_values", "{}")
+
+    # Extract the message from the POST request body
+    text: str = data.get("text", "")
+
+    print("********************************** analyser_demande **********************************")
+    return app.api.analyze(field_values, text)
+
+@app.post(f"{API_ROUTE}/process_request", tags=["Services"])
+async def handle_case_v1(data: dict):
+    # Extract the case request object from the POST request body
+    case_request= data.get("case_request", "{}")
+    if isinstance(case_request, dict):
+        case_request = CaseHandlingRequest(**case_request)
+
+    print ("********************************** handle_case_v1 **********************************")
+    return app.api.handle_case(case_request)
+
