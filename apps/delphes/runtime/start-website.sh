87ee70c3815a6d4489f71371a631ca6253feda96
Joël Milgram <joel@milgram.fr>
2025-09-17 14:23:48 +0200

    Update website script

--- apps/delphes/runtime/start-website.sh
+++ apps/delphes/runtime/start-website.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define the port number
-PORT=5005
+PORT=5006
 
 # Navigate to the website directory
 cd website || exit
 
 # Start a simple HTTP server on localhost
-python -m http.server "$PORT" --bind 127.0.0.1 || {
+python3.12 -m http.server "$PORT" --bind 127.0.0.1 || {
   echo -e "\033[31mError: Port $PORT is already in use.\033[0m"
   exit 1
 }
