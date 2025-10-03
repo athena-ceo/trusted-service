ad81bbc044981d29a970ab80155f52c59ad97a01
Francis Athena <ffriedlander2012@gmail.com>
2025-10-03 08:26:29 +0200

    Complete Finnish localization for use case 1

--- src/backend/distribution/distribution_email/distribution_email_localization.py
+++ src/backend/distribution/distribution_email/distribution_email_localization.py
@@ -13,19 +13,23 @@ class DistributionEngineEmaiLocalization(Localization):
 # IF YOU CHANGE THE FOLLOWING COMMENT, UPDATE README.md ACCORDINGLY
 # Add here support for new languages
 distribution_engine_email_localizations: dict[SupportedLocale, DistributionEngineEmaiLocalization] = {
+
     "en": DistributionEngineEmaiLocalization(
         label_intent="Intent",
         label_yes="yes",
         label_no="no",
         label_notes="Important notes"),
+
     "fr": DistributionEngineEmaiLocalization(
         label_intent="Intention",
         label_yes="oui",
         label_no="non",
         label_notes="Eléments importants"),
+
     "fi": DistributionEngineEmaiLocalization(
-        label_intent="Intent",
-        label_yes="yes",
-        label_no="no",
-        label_notes="Important notes"),
+        label_intent="aikomus",
+        label_yes="kyllä",
+        label_no="ei",
+        label_notes="tärkeät muistiinpanot"),
+
 }
